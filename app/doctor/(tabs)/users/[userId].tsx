8baa7b5a0f829eb29021f5ce85df8d468477499b
OllieDarwin <oliverdarwin365@gmail.com>
2025-01-04 20:29:40 +0000

    Added more user information to user profiles.

--- app/doctor/(tabs)/users/[userId].tsx
+++ app/doctor/(tabs)/users/[userId].tsx
@@ -1,7 +1,7 @@
 import { getUserFromId, User } from "@/firebase/auth";
 import { useSearchParams } from "expo-router/build/hooks";
 import { useEffect, useState } from "react";
-import { View, Text } from "react-native";
+import { View, Text, ScrollView } from "react-native";
 
 export default function UserProfile() {
     const searchParams = useSearchParams()
@@ -10,7 +10,6 @@ export default function UserProfile() {
     const [loading, setLoading] = useState(true)
 
     useEffect(() => {
-        console.log(userId)
         const fetchUser = async () => {
             if (!userId) return
 
@@ -29,8 +28,36 @@ export default function UserProfile() {
     if (!user) return <Text>User not found</Text>
 
     return (
-        <View className="p-4">
-            <Text>{user.firstName} {user.lastName}</Text>
-        </View>
+        <ScrollView className="p-4 bg-gray-100">
+            <View className="bg-white w-full py-6 px-4 rounded-lg flex flex-row gap-4 items-center">
+                <View className="bg-gray-200 w-16 h-16 rounded-full"></View>
+                <View>
+                    <Text className="text-2xl font-semibold">{user.firstName} {user.lastName}</Text>
+                    <Text className="text-gray-700 text-lg">{user.email}</Text>
+                    <Text className="text-gray-700 text-lg">{user.phone}</Text>
+                    <Text className="text-gray-700 text-lg">{user.dateOfBirth}</Text>
+                </View>
+            </View>
+            <View className="bg-white w-full py-6 px-4 rounded-lg flex flex-row gap-4 items-center mt-4">
+                <View>
+                    <Text className="text-2xl font-bold">Address</Text>
+                    <View className="mt-2">
+                        <Text className="text-gray-700 text-base">{user.address?.addressLine1}</Text>
+                        { user.address?.addressLine2 !== "" && <Text className="text-gray-700 text-base">{user.address?.addressLine2}</Text> }
+                        <Text className="text-gray-700 text-base">{user.address?.city}</Text>
+                        <Text className="text-gray-700 text-base">{user.address?.country}</Text>
+                        <Text className="text-gray-700 text-base">{user.address?.postalCode}</Text>
+                    </View>
+                </View>
+            </View>
+            <View className="bg-white w-full py-6 px-4 rounded-lg mt-4">
+                <Text className="text-2xl font-bold">Emergency Contact</Text>
+                <View className="mt-2">
+                    <Text className="text-gray-700 text-base">{user.emergencyContact?.name}</Text>
+                    <Text className="text-gray-700 text-base">{user.emergencyContact?.phone}</Text>
+                    <Text className="text-gray-700 text-base">{user.emergencyContact?.relationship}</Text>
+                </View>
+            </View>
+        </ScrollView>
     )
 }
